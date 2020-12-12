80a770b7de95298d6fd1a4ed75112fffe0c57569
Mho9804 <Mho9804@conestoga.on.ca>
2020-12-12 01:45:57 +0000

    Fix critical error for protocol_connection_lost

--- dev/lib/db.js
+++ dev/lib/db.js
@@ -1,15 +1,29 @@
 var mysql=require('mysql');
- var connection=mysql.createConnection({
-   host:'localhost',
-   user:'root',
-   password:'123456',
-   database:'seng8040'
- });
-connection.connect(function(error){
-   if(!!error){
-     console.log(error);
-   }else{
-     console.log('Connected!:)');
-   }
- });  
+
+function handleDisconnect() {
+  var connection=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'123456',
+    database:'seng8040'
+  });
+  connection.connect(function(err) {              
+      if(err) {                     
+          console.log('error when connecting to db:', err);
+          setTimeout(handleDisconnect, 2000); 
+      }                                
+  });                             
+  connection.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+          console.log('FIXING PROTOCOL_CONNECTION_LOST')
+          handleDisconnect();              
+      } else {              
+          throw err;      
+      }
+  });
+  return connection;
+}
+
+var connection = handleDisconnect();
 module.exports = connection; 
